6887b3f7ba9ada0e9756ea18ce2bae9266472771
Jessica Washington <jessicaswashington@gmail.com>
2014-07-28 18:31:08 -0400

    first commit

---
+++ temperature.rb
@@ -0,0 +1,25 @@
+# todays_temperature = 80
+
+# if todays_temperature < 85
+
+# 	puts "I'm going hiking!"
+
+# end
+
+
+# if 10 != 5+5
+
+# 	puts "Ten is still ten! All right!"
+
+# end
+
+
+def going_hiking(temp)
+
+	if temp is >= 50
+
+		puts "#{temp} is perfect for hiking!"
+
+	end
+end
+going_hiking(80)
